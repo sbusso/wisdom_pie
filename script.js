2f8ddba0dfe0ad1c1c5106d6d389a6951b24cdb7
Stéphane Busso <stephane.busso@gmail.com>
2014-09-06 11:35:25 +0700

    bouncing wheel

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+Raphael.fn.pieChart = function (cx, cy, r, values, labels, stroke) {
+    var paper = this,
+        rad = Math.PI / 180,
+        chart = this.set();
+    function sector(cx, cy, r, startAngle, endAngle, params) {
+        var x1 = cx + r * Math.cos(-startAngle * rad),
+            x2 = cx + r * Math.cos(-endAngle * rad),
+            y1 = cy + r * Math.sin(-startAngle * rad),
+            y2 = cy + r * Math.sin(-endAngle * rad);
+        return paper.path(["M", cx, cy, "L", x1, y1, "A", r, r, 0, +(endAngle - startAngle > 180), 0, x2, y2, "z"]).attr(params);
+    }
+    var angle = 0,
+        total = 0,
+        start = 0,
+        process = function (j) {
+            var value = values[j],
+                angleplus = 360 * value / total,
+                popangle = angle + (angleplus / 2),
+                color = Raphael.hsb(start, .75, 1),
+                ms = 500,
+                delta = 30,
+                bcolor = Raphael.hsb(start, 1, 1),
+                p = sector(cx, cy, r, angle, angle + angleplus, {fill: "90-" + bcolor + "-" + color, stroke: stroke, "stroke-width": 3}),
+                txt = paper.text(cx + (r + delta + 55) * Math.cos(-popangle * rad), cy + (r + delta + 25) * Math.sin(-popangle * rad), labels[j]).attr({fill: bcolor, stroke: "none", opacity: 0, "font-size": 20});
+            p.mouseover(function () {
+                p.stop().animate({transform: "s1.1 1.1 " + cx + " " + cy}, ms, "elastic");
+                txt.stop().animate({opacity: 1}, ms, "elastic");
+            }).mouseout(function () {
+                p.stop().animate({transform: ""}, ms, "elastic");
+                txt.stop().animate({opacity: 0}, ms);
+            });
+            angle += angleplus;
+            chart.push(p);
+            chart.push(txt);
+            start += .1;
+        };
+    for (var i = 0, ii = values.length; i < ii; i++) {
+        total += values[i];
+    }
+    for (i = 0; i < ii; i++) {
+        process(i);
+    }
+    return chart;
+};
+
 (function () {
     //Variables - following single variable pattern
     var creativity_color = '#ffff00', //yellow
@@ -18,8 +63,8 @@
         system_value = 21,
         pieData = [creativity_value, communication_value, person_value, thorough_value, analytic_value, system_value],
 
-        startAngle = 270,
-        endAngle = 270,
+        startAngle = -90,
+        endAngle = -90,
         sectorAngle = 0,
         total = 0,
         radius = 150,
@@ -27,6 +72,7 @@
         cx = 200,
         cy = 200,
         animSpeed = 500,
+        ms = 500,
         paper = Raphael('canvas'),
         path = null;
 
@@ -62,9 +108,15 @@
         endAngle = startAngle + sectorAngle;  //the end angle for each pie slice
 
         //create a rapahel path with the custom pie 'sector' attribute that we defined above. Start angle initially is 0
-        path = paper.path().attr({sector:[cx, cy, radius, 0, 0], stroke:'#fff', "stroke-width":1, "stroke-linejoin" : "round","stroke-linecap" : "round", fill:colorArr[k]}).data("id", k).click(function() {
+        path = paper.path().attr({sector:[cx, cy, radius, 0, 0], stroke:'#fff', "stroke-width":0, "stroke-linejoin" : "round","stroke-linecap" : "round", fill:colorArr[k]}).data("id", k).click(function() {
             alert(this.data("id")); //I have also added a click event for each slice. This is just in case you need to handle a pie slice click
-        });
+        }).mouseover(function () {
+                this.stop().animate({transform: "s1.1 1.1 " + cx + " " + cy}, ms, "elastic");
+                // txt.stop().animate({opacity: 1}, ms, "elastic");
+            }).mouseout(function () {
+                this.stop().animate({transform: ""}, ms, "elastic");
+                // txt.stop().animate({opacity: 0}, ms);
+            });
 
         //change the pie sector attribute over time - this is where the animation happens
         path.animate({sector:[cx, cy, radius, startAngle, endAngle]}, animSpeed, function() {
