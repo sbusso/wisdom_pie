be48aa41ca9d8de7bcaafe71f7197f396afe0ff5
Stéphane Busso <stephane.busso@gmail.com>
2014-09-06 11:12:47 +0700

    stephane wheel

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 (function () {
     //Variables - following single variable pattern
-    var colorArr = [ '#bca37d','#94ca23','#fcf579','#fdda2a','#8adbd0','#4dbeaa','#945a13','#fdb724','#7eac1a','#f28d36','#917f6d','#b3f2c4'], //sample color data
-        pieData = [95, 60, 38, 45, 23, 56, 88, 120, 73], //sample data        
-        startAngle = 0,
-        endAngle = 0,
+    var creativity_color = '#ffff00', //yellow
+        communication_color = '#ff8000', //orange
+        person_color = '#ff0000', //red
+        thorough_color = '#8000ff', //purple
+        analytic_color = '#0000ff', //blue
+        system_color = '#00ff00', //green
+
+
+        colorArr = [ creativity_color, communication_color, person_color, thorough_color, analytic_color, system_color ], //sample color data
+
+        creativity_value = 21,
+        communication_value = 0,
+        person_value = 6,
+        thorough_value = 15,
+        analytic_value = 37,
+        system_value = 21,
+        pieData = [creativity_value, communication_value, person_value, thorough_value, analytic_value, system_value],
+
+        startAngle = 270,
+        endAngle = 270,
         sectorAngle = 0,
-        total = 0,        
-        radius = 150, 
-        donutRadius = 70,
+        total = 0,
+        radius = 150,
+        donutRadius = 0,
         cx = 200,
         cy = 200,
         animSpeed = 500,
         paper = Raphael('canvas'),
-        path = null; 
+        path = null;
+
+
+     //sample data
+
 
     //creates a custom attribute 'sector' for a Rapahel path
     paper.customAttributes.sector = function(cx, cy, r, startAngle, endAngle) {
@@ -25,19 +45,19 @@
 
         return {
             path:[["M", cx, cy,],["L", x1, y1,],["A", r, r, 0, +flag, 1, x2, y2,],["z"]]
-        }
-    }
+        };
+    };
 
 
     //CALCULATE THE TOTAL
     for(var k=0; k < pieData.length; k++){
         total += pieData[k];
     }
-    
+
     //CALCULATE THE ANGLES THAT EACH SECTOR SWIPES AND STORE IN AN ARRAY
     for(k=0; k < pieData.length; k++){
-        sectorAngle = 360 * pieData[k]/total;   //calculate the sector angle for a pie slice
-        
+        sectorAngle = (360 * pieData[k]/total);   //calculate the sector angle for a pie slice
+
         startAngle = endAngle;  //the start angle for each pie slice
         endAngle = startAngle + sectorAngle;  //the end angle for each pie slice
 
@@ -49,9 +69,9 @@
         //change the pie sector attribute over time - this is where the animation happens
         path.animate({sector:[cx, cy, radius, startAngle, endAngle]}, animSpeed, function() {
             console.log('callback function called after animation');
-        });          
-    }    
-    
+        });
+    }
+
     //create the donut for the pie chart. If you don't need it then remove this part
     paper.circle(cx, cy, donutRadius).attr({"fill":"#fff", "stroke": "#fff"});
 })();
